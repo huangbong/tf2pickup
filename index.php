caadfb2df85d06637ed33038898405656ae6d15c
frailbod <mitchell@domU-12-31-39-0A-34-83.compute-1.internal>
2012-02-21 23:57:34 +0000

    chat is even better

--- index.php
+++ index.php
@@ -9,13 +9,14 @@ $steamloginbutton = '<a href="' . SteamSignIn::genUrl() . '"><img src="http://cd
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<title>Team Fortress 2 Pickup</title>
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
 </head>
 
 <body>
 <div id="wrapper">
 	<div id="header">
 		<div id="welcome">
-<div id="logo"></div>
+			<div id="logo"></div>
 		</div>
 		<div id="steam">
 		<?php include 'userbar.php';?>
@@ -27,15 +28,19 @@ $steamloginbutton = '<a href="' . SteamSignIn::genUrl() . '"><img src="http://cd
 			2.19.2012 - haxing noobs
 			</div>
 			<div id="chat">
-			falcro: i'm gey
+				<?php include 'chat/chat.php';?>
 			</div>
 		</div>
 		<div id="right">
 			<div class="pug">
+				<div class="pug1">
 			6v6 Badlands
 			<br />cp_badlands
 			<br /><div class="flag"><img src="img/us.png" alt="us flag"/> US</div>
+				</div>
+			<div class="pug2">
 			15/18
+			</div>
 			</div>
 			<div class="pug">
 			highlander lulz
