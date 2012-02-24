f01b07fe9db8e07a3d599a5cf115b97fa3e8121e
Gcommer <gcommer@gmail.com>
2012-02-23 23:06:10 -0500

    Fixed? verify.php

--- verify.php
+++ verify.php
@@ -5,6 +5,13 @@ require_once 'mysql.php';
 require_once 'geoip/geoip.php';
 
 $steam64 = SteamSignIn::validate();
+
+if ($steam64 === false)
+{
+    header("Location: /");
+    exit(0);
+}
+
 $steamapi = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=2C90B16410B5B8BE3DB9A7FD67A76A89&steamids=" . $steam64);
 $json = json_decode($steamapi);
 $username = $json->response->players[0]->personaname;
@@ -14,13 +21,11 @@ $country = findme::country();
 
 $con = mysql::connect();
 
-$search = "SELECT * FROM `users` WHERE `id`=$steam64";
+$search = "SELECT * FROM `users` WHERE `id` = '$steam64'";
 $result = mysql_query($search);
 
 if (mysql_num_rows($result) >= 0) {
-    mysql_query("UPDATE users SET username = '$username' WHERE id = '$steam64'");
-    mysql_query("UPDATE users SET avatar = '$avatar' WHERE id = '$steam64'");
-    mysql_query("UPDATE users SET country = '$country' WHERE id = '$steam64'");
+    mysql_query("UPDATE `users` SET `username` = '$username', `avatar` = '$avatar', `country` = '$country' WHERE `id` = '$steam64'");
 }
 else {
     mysql_query("INSERT INTO users (id, username, avatar, country)
