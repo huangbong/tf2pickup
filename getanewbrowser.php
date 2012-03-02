8a5702889f892d06622c20ca641c491311910e31
mitchell <mitchell@debian.(none)>
2012-03-02 16:25:57 -0500

    moved things to cdn, fixed getanewbroswer css

--- getanewbrowser.php
+++ getanewbrowser.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
@@ -8,25 +7,29 @@
         <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
         <script src="http://code.jquery.com/jquery-latest.js"></script>
         <script src="scripts/jsrender.js"></script>
+<style media="screen" type="text/css">
+#notification {
+    padding-top: 10%;
+}
+
+</style>
     </head>
     <body>
 
         <div id="wrapper">
             <div id="header">
                 <div id="logo"></div>
-                <div id="steam">
-                </div>            </div>
+                <div id="steam"></div>
+            </div>
 
             <div id="middle">
-                <div id="pugs_container">
+                <div id="notification">
                 <h2>Your browser is not supported by TF2Pickup.</h2>We have support for Internet Explorer 9+, Firefox, Chrome, and Opera.<br/>We strongly recommend installing <a href="http://www.google.com/chrome">Google Chrome</a> or <a href="http://www.mozilla.org/en-US/firefox/new/">Mozilla Firefox</a> before using TF2Pickup.
                 </div>
                 <div id="footer">
                     &copy; 2012 TF2Pickup.com
                 </div>
             </div>
-
-        </div>
     </body>
 </html>
 
