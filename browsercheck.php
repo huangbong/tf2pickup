d2b8576c3052b838a08ca7782868371cd1107e3e
Gcommer <gcommer@gmail.com>
2012-03-01 23:15:10 -0500

    Fixing frail's bad PHP

--- browsercheck.php
+++ browsercheck.php
@@ -1,8 +1,8 @@
 <?php
 $get_browser = get_browser(null, true);
 
-$browser = $get_browser[browser];
-$version = $get_browser[version];
+$browser = $get_browser["browser"];
+$version = $get_browser["version"];
 
 if ($browser == "IE" and $version < 9) {
 header('Location: getanewbrowser.php');
