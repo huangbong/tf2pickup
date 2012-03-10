69454be3c62b5777fcde8d9ec0142752e567fd94
Gcommer <gcommer@gmail.com>
2012-03-10 01:09:21 -0500

    Add getFriends.php

---
+++ ajax/getFriends.php
@@ -0,0 +1,21 @@
+<?php
+// steam condenser is borked... so just in case
+error_reporting(0);
+ini_set('display_errors', '0');
+
+require_once dirname(__FILE__).'/../session.php';
+require_once dirname(__FILE__).'/../steam-condenser/steam-condenser.php';
+
+if (!isset($_SESSION['steam64']))
+    die ("Not authenticated!");
+
+try {
+    $user = new SteamId($_SESSION['steam64']);
+    $friends = $user->getFriends();
+} catch (SteamCondenserException $e) {die("");}
+
+$steamids = array();
+foreach ($friends as $friend) {
+    array_push($steamids, $friend->getSteamID64());
+}
+echo json_encode($steamids);
