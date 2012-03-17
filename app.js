64ef725bf2b5b1707d97473028b5e0b62a3730d9
Gcommer <gcommer@gmail.com>
2012-03-17 10:41:25 -0400

    Initial Express layout

---
+++ app.js
@@ -0,0 +1,36 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'tf2pickup949172463r57276' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', function (req, res) {
+  res.render('index', { logged_in: false });
+});
+
+app.listen(80);
+console.log("Express server running tf2pickup on port %d in %s mode", app.address().port, app.settings.env);
