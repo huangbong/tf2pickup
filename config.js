87bf23d4f20fcd3154ae19060687587d354dc1b5
Gcommer <gcommer@gmail.com>
2012-03-25 23:30:02 -0400

    port config.php -> config.js

--- config.js
+++ config.js
@@ -1,6 +1,16 @@
 module.exports = {
   port: 80,
-  host_name: 'dev.tf2pickup.com:3000',
-  steam_api_key = '2C90B16410B5B8BE3DB9A7FD67A76A89'
+  host_name: 'dev.tf2pickup.com',
 
+  steam_api_key: '2C90B16410B5B8BE3DB9A7FD67A76A89',
+  steam_avatar_base_url: 'http://media.steampowered.com/steamcommunity/public/images/avatars/',
+
+  geoip_db_file: '/usr/local/share/GeoIP/GeoLiteCity.dat',
+
+  db: {
+    host: 'localhost',
+    name: 'tf2pickup',
+    user: 'tf2pickup',
+    pass: 'dHuY4k3MNflJ6RV'
+  }
 };
