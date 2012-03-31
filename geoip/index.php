9c8e0ec30efa42dc5fe71c7ceac0d44a3b7a3bf1
Gcommer <gcommer@gmail.com>
2012-03-31 15:13:52 -0400

    Remove PHP geoip

--- geoip/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/geoip/geoip.php';
-
-GeoIP::loadData();
-
-echo 'You are in: ' . GeoIP::getCity() . ',' . GeoIP::getCountry();
-echo '<br />';
-GeoIP::map(600,300);
-
